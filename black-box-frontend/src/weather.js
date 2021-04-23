afab15975d573711742ed528a545c958feca5632
RichardRios <richierios007@gmail.com>
2021-04-22 21:15:05 -0400

    weather is rendering

--- black-box-frontend/src/weather.js
+++ black-box-frontend/src/weather.js
@@ -8,22 +8,26 @@ export default class Weather extends Component {
         this.state = {
             vaca: this.props.vaca,
             query: this.props.vaca.location,
-            weather: []
+            weather: {}
         }
     }
 
-    fetchWeather = () =>{
-       fetch(`https://community-open-weather-map.p.rapidapi.com/forecast?q=${this.state.query}`, {
+    fetchWeather= () =>{
+        // console.log('before call' ,this.state.query, this.state.vaca.dateFrom)
+       fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${this.state.query}&days=5&dt=${this.state.vaca.dateFrom}`, {
             "method": "GET",
             "headers": {
-            "x-rapidapi-key": "5168948a50msh664513c5262f841p1e6c29jsn7b3a056e3cea",
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+                "x-rapidapi-key": "5168948a50msh664513c5262f841p1e6c29jsn7b3a056e3cea",
+                "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
             }
         })
         .then(response => response.json())
-        .then(json => {
-            
-            console.log(' response', json)
+        .then(json=> {
+            console.log('first response log', json)
+            this.setState({
+                weather: json.forecast.forecastday[0]
+              })  
+            console.log('after it has been set', this.state.weather)
         })
         .catch(err => {
             console.error(err);
@@ -37,9 +41,22 @@ export default class Weather extends Component {
 
 
     render() {
-        console.log('weather in r', this.state.weather)
+        console.log('weather in render', this.state.weather.day.condition)
+
         return (
-          <h1> weather </h1>
+            <div>
+            <h1> {this.state.weather.date}</h1>
+           
+            </div>
+          // <div className= 'weather-container'>
+          //   {this.state.weather.map(info=> {
+        //         return(
+        //             <div className= 'info' key= {info._id}>
+        //                 <h1> {info.date} </h1>
+        //             </div>
+        //         )
+        //     })}
+        //   </div>
         ) 
     }            
 
